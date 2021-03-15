effc01d021a63b18df293d09e648006ef40b7fa7
Davidson Veniali <estudosdavidson@gmail.com>
2021-03-15 20:38:46 -0300

    Add files via upload

---
+++ ExericicioCaixa/src/main/java/com/mycompany/exericiciocaixa/CaixaMain/CaixaMain.java
@@ -0,0 +1,10 @@
+package com.mycompany.exericiciocaixa.CaixaMain; 
+public class CaixaMain {
+ 
+    public static void main(String[] args) {
+
+        int valor = 250;
+        Saque saque = new Saque(valor);
+     
+    };
+};
